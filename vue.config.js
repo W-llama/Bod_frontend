25be2ca25a3739ba0b67f385f6222997f07cbe8e
W.llama <tmdqja96@gmail.com>
2024-08-12 10:14:03 +0900

    Dockerfile 변경 nginx.conf 변경 CI/CD를 위해 docker-image.yml

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,27 @@
-// vue.config.js
-
-const { DefinePlugin } = require('webpack');
-
-module.exports = {
-    devServer: {
-        port: 8081,
-        proxy: {
-            '/api': {
-                target: 'http://3.37.71.106:8080',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    },
-    configureWebpack: {
-        plugins: [
-            new DefinePlugin({
-                __VUE_PROD_DEVTOOLS__: 'false',
-                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-                'process.env.VUE_APP_API_URL': JSON.stringify(process.env.NODE_ENV === 'production' ? 'http://challengersbod.store' : 'http://localhost:8080')
-            })
-        ]
-    }
-};
+// // vue.config.js
+//
+// const { DefinePlugin } = require('webpack');
+//
+// module.exports = {
+//     devServer: {
+//         port: 8081,
+//         proxy: {
+//             '/api': {
+//                 target: 'http://3.37.71.106:8080',
+//                 changeOrigin: true,
+//                 pathRewrite: {
+//                     '^/api': ''
+//                 }
+//             }
+//         }
+//     },
+//     configureWebpack: {
+//         plugins: [
+//             new DefinePlugin({
+//                 __VUE_PROD_DEVTOOLS__: 'false',
+//                 __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+//                 'process.env.VUE_APP_API_URL': JSON.stringify(process.env.NODE_ENV === 'production' ? 'http://challengersbod.store' : 'http://localhost:8080')
+//             })
+//         ]
+//     }
+// };
