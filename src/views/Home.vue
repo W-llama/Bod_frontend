783652eca2aa8637c65f23ae720f050dadc69609
kinam <maltize9292@gmail.com>
2024-08-14 16:08:12 +0900

    refactor / 메인에 사이트 이용방법 기재

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <PointRankingSection />
+    <div style="display: flex; align-items: center;">
+      <PointRankingSection />
+      <h3 style="color: #ffffff;">
+        CHALLENGERS-B.O.D 사이트 이용방법~!<br>
+        1. 회원가입 및 로그인~!<br>
+        2. 챌린지 참여하기에서 원하는 챌린지 선택해서 참가하기~!<br>
+        3. 마이페이지에서 챌린지 인증하기~!<br>
+        4. 챌린지 인증이 수락되면 500 point up~!<br>
+        5. 챌린지 랭킹 상위권에게는 생각지도 못한 보상이 있을지도~!
+      </h3>
+    </div>
     <ChallengeCard />
   </div>
 
